1e30de725e35f1d2efd027eea4e1a4d46054c99e
ronaldpedra <ronaldpedra@gmail.com>
2024-01-27 22:22:32 -0300

    finalizando o mundo 1

--- Galáxia 5/mundo_1_estrutura.py
+++ Galáxia 5/mundo_1_estrutura.py
@@ -1,7 +1,7 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
-from mundo_1_import import *
+from mundo_1_import import contar_goiabas
 
 '''
 DADOS
@@ -11,24 +11,27 @@ CALLBACKS
 RODAR O APP
 '''
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-app = Dash(__name__) # Esse é o nome do seu código
+app = Dash(__name__)  # Esse é o nome do seu código
 
 app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    html.H1(children='Crescimento Populacional no Mundo',
+            style={'textAlign': 'center'}),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ])
+
 
 @callback(
     Output('graph-content', 'figure'),
     Input('dropdown-selection', 'value')
 )
-
 def update_graph(value):
-    dff = df[df.country==value]
+    dff = df[df.country == value]
     return px.line(dff, x='year', y='pop')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Galáxia 5/mundo_1_import.py
@@ -0,0 +1,7 @@
+def contar_goiabas(goiabas):
+    print(f'O usuário tem {goiabas} goiabas')
+
+
+if __name__ == '__main__':
+    contar_goiabas(2)
+    print('Já passou!')
