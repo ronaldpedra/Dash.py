bc086587b87d338921d294d0eafe11a7a46656a5
ronaldpedra <ronaldpedra@gmail.com>
2024-01-27 21:43:50 -0300

    complemento do arquivo

--- Galáxia 5/mundo_1_estrutura.py
+++ Galáxia 5/mundo_1_estrutura.py
@@ -20,3 +20,16 @@ app.layout = html.Div([
       dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
       dcc.Graph(id='graph-content')
 ])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    contar_goiabas(2)
